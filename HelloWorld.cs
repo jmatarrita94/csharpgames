ae3ae5de2bea6a9862da15c7942a920876c3fc74
Jmatarrita <josemmatarritacampos@gmail.com>
2024-12-08 18:57:09 -0600

    initial commit

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+class Hello
+{
+    public Hello()
+    {
+
+    }
+
+    public void printsHello()
+    {
+        // This line prints "Hello, World" 
+        Console.WriteLine("Hello, World mine");
+    }
+    
+}
